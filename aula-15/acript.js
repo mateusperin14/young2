ea36db7f021216c50a1857ae9728e34b91fc5733
mateusperin14 <mateusgperin@gmail.com>
2024-09-11 17:26:27 -0300

    aula-15

---
+++ aula-15/acript.js
@@ -0,0 +1,14 @@
+const SENHA_ELEMENTO = document.querySelector('#password')
+const VISOR_ELEMENTO = document.querySelector('.password-dsplay')
+const TAMANHO_ELEMENTO = document.querySelector('#tamanho')
+const MAIUSCULA_ELEMENTO = document.querySelector('#maiusculas')
+const MINUSCULA_ELEMENTO = document.querySelector('#minusculas')
+const NUMEROS_ELEMENTO = document.querySelector('#numeros')
+const CARACTERES_ELEMENTO = document.querySelector('#caracteres')
+const GERAR_ELEMENTO = document.querySelector('.submit')
+const LIMPAR_ELEMENTO = document.querySelector('.reset')
+
+const MAIUSCULAS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const MINUSCULAS = MAIUSCULAS.toLowerCase()
+const NUMEROS = '0123456789'
+const CARACTERES = '!@#$%¨&*()+=_;:;ç'
